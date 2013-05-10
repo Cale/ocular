41c29ea17efdf161ca8422f5c742fcb4ecef43c7
Cale Mooth <cale@midnightcheese.com>
2013-05-09 21:02:26 -0500

    Redeploy to Heroku.

--- ocular.js
+++ ocular.js
@@ -323,7 +323,7 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
   Meteor.startup( function () {
-    // code to run on server at startup
+    // code to run on server at startup.
   });
 
   Meteor.publish("feeds", function () {
